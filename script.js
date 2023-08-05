9702b59a0d962609cf01fb20c2b84ad515c5760f
Vineeth <89705628+veeinmaking@users.noreply.github.com>
2023-08-05 14:55:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let input = document.querySelector("#inp");
+let ul = document.querySelector("#list");
+
+input.addEventListener("keypress", function (e) {
+  if (e.code == "Enter") {
+    let task = e.currentTarget.value;
+    task = task.trim();
+    if (task == "") return;
+    let li = document.createElement("li");
+    li.innerHTML = task;
+
+    li.addEventListener("dblclick", function (e) {
+      e.currentTarget.remove();
+    });
+    ul.append(li);
+    e.currentTarget.value = "";
+  }
+});
